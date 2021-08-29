bbacba4bafbd3cbbdf4f08531741a0000fcea08b
mateuskuritza <mateuskuritza@gmail.com>
2021-08-29 18:48:02 -0300

    remove console.log

--- src/fakeAPI.ts
+++ src/fakeAPI.ts
@@ -2,10 +2,9 @@ import fakeData from "./interfaces/fakeData"
 
 export default async function getData(): Promise<fakeData[]> {
     const getUrl = window.location;
-    const baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[0];
-    console.log(baseUrl);
+    const baseUrl = getUrl.protocol + "//" + getUrl.host;
     const result = await fetch(
-        `${baseUrl}data.json`,
+        `${baseUrl}/data.json`,
         {
             headers:
                 { 'Content-Type': 'application/json', 'Accept': 'application/json' }
